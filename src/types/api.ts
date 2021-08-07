5c82bf0611a895f00416d4ac5ddded61f4344739
dougluz <dougs.2210@gmail.com>
2021-08-06 23:58:17 -0300

    feat: api related types now have a namespace

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,42 +1,45 @@
-export type Photo = {
-  id: number
-  width: number
-  height: number
-  url: string
-  photographer: string
-  photographer_url: string
-  photographer_id: number
-  avg_color: string
-  src: {
-    original: string
-    large2x: string
-    large: string
-    medium: string
-    small: string
-    portrait: string
-    landscape: string
-    tiny: string
-  }  
-}
+export namespace API {
+  export type Photo = {
+    id: number
+    width: number
+    height: number
+    url: string
+    photographer: string
+    photographer_url: string
+    photographer_id: number
+    avg_color: string
+    src: {
+      original: string
+      large2x: string
+      large: string
+      medium: string
+      small: string
+      portrait: string
+      landscape: string
+      tiny: string
+    }  
+  }
+  
+  export type ParsedPexelPhotosResponse = {
+    nextPage: string
+    page: number
+    perPage: number
+    photos: Array<Photo>
+  }
+  
+  export type PexelPhotosResponse = {
+    next_page: string,
+    page: number,
+    per_page: number,
+    photos: Array<Photo>
+  }
+  
+  export type PexelFullResponse = {
+    config: {},
+    data: PexelPhotosResponse
+    headers: {}
+    status: number
+    statusText: string
+  }
 
-export type ParsedPexelPhotosResponse = {
-  nextPage: string
-  page: number
-  perPage: number
-  photos: Array<Photo>
-}
-
-export type PexelPhotosResponse = {
-  next_page: string,
-  page: number,
-  per_page: number,
-  photos: Array<Photo>
-}
-
-export type PexelFullResponse = {
-  config: {},
-  data: PexelPhotosResponse
-  headers: {}
-  status: number
-  statusText: string
 }
